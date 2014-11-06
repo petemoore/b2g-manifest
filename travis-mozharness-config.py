4085cd70e8749062a1e38ab127fd783fa5a149d0
Pete Moore <pmoore@mozilla.com>
2014-11-06 20:00:18 +0100

    Corrected location of web.cacerts

--- travis-mozharness-config.py
+++ travis-mozharness-config.py
@@ -2,7 +2,7 @@
 config = {
     "exes": {
         # Get around the https warnings
-        "hg": ['/usr/local/bin/hg', "--config", "web.cacerts=/Users/pmoore/ca-bundle.crt"],
+        "hg": ['/usr/local/bin/hg', "--config", "web.cacerts=/etc/ssl/certs/ca-certificates.crt"],
         "hgtool.py": ["HGTOOL"],
         "gittool.py": ["GITTOOL"],
     },
