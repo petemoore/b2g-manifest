0a7ba2845750312d2b5dfb4200eea66f8dfa4223
Pete Moore <pmoore@mozilla.com>
2014-11-06 16:52:52 +0100

    Work in progress for bug 1091066

---
+++ validate.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -euxv
+
+set +e
+ls /etc/ssl/certs/ca-certificates.crt
+ls /etc/pki/tls/certs/ca-bundle.crt
+ls /etc/ssl/ca-bundle.pem
+set -e
+
+echo "Installing mozharness..."
+B2GBUMPER_DIR="$(pwd)"
+cd ..
+echo "Current directory: '$(pwd)'"
+git clone -b bug1091084 https://github.com/petemoore/build-mozharness mozharness
+echo "Installing tools..."
+git clone https://github.com/mozilla/build-tools tools
+
+HGTOOL_PATH="$(find "$(pwd)/tools" -name hgtool.py)"
+GITTOOL_PATH="$(find "$(pwd)/tools" -name gittool.py)"
+
+function replace {
+    local file="${1}"
+    local tokenname="${2}"
+    local replacestring="${3}"
+    # don't use sed -i so we can test on mac
+    mv "${file}" "${file}.orig"
+    cat "${file}.orig" | sed "s*${tokenname}*${replacestring}*g" > "${file}"
+    rm "${file}.orig"
+}
+
+# update references to hgtool.py and gittool.py to correct locations...
+replace "${B2GBUMPER_DIR}/travis-mozharness-config.py" HGTOOL "${HGTOOL_PATH}"
+replace "${B2GBUMPER_DIR}/travis-mozharness-config.py" GITTOOL "${HGTOOL_PATH}"
+
+mozharness/scripts/b2g_bumper.py -c "${B2GBUMPER_DIR}/travis-mozharness-config.py" --no-check-treestatus
+
+echo "Running CI tests..."
