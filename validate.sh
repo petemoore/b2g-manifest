a2e5bed18fccd324817442e7c598085fc01db939
Pete Moore <pmoore@mozilla.com>
2014-11-07 10:10:13 +0100

    fix

--- validate.sh
+++ validate.sh
@@ -1,5 +1,18 @@
 #!/bin/bash -euxv
 
+function replace {
+    local file="${1}"
+    local tokenname="${2}"
+    local replacestring="${3}"
+    # don't use sed -i so we can test on mac
+    mv "${file}" "${file}.orig"
+    cat "${file}.orig" | sed "s@${tokenname}@${replacestring}@g" > "${file}"
+    rm "${file}.orig"
+}
+
+find . -name '*.xml' | while read file; do
+    replace "${file}" '^.*path="gaia".*' '<!-- filter out gaia for CI test & -->'
+done
 B2GBUMPER_DIR="$(pwd)"
 cd ..
 echo "Installing mozharness..."
@@ -10,16 +23,6 @@ git clone https://github.com/mozilla/build-tools tools
 
 HGTOOL_PATH="$(find "$(pwd)/tools" -name hgtool.py)"
 GITTOOL_PATH="$(find "$(pwd)/tools" -name gittool.py)"
-
-function replace {
-    local file="${1}"
-    local tokenname="${2}"
-    local replacestring="${3}"
-    # don't use sed -i so we can test on mac
-    mv "${file}" "${file}.orig"
-    cat "${file}.orig" | sed "s*${tokenname}*${replacestring}*g" > "${file}"
-    rm "${file}.orig"
-}
 
 echo "Replacing references to hgtool.py and gittool.py to versions checked out in tools repo..."
 replace "${B2GBUMPER_DIR}/travis-mozharness-config.py" HGTOOL "${HGTOOL_PATH}"
