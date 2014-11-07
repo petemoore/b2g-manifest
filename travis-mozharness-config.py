5cb55c9c3e1ad62dc816609e1741ed58be06e632
Pete Moore <pmoore@mozilla.com>
2014-11-07 18:46:34 +0100

    stuff

--- travis-mozharness-config.py
+++ travis-mozharness-config.py
@@ -6,8 +6,6 @@ config = {
         "hgtool.py": ["HGTOOL"],
         "gittool.py": ["GITTOOL"],
     },
-    'gecko_pull_url': 'https://hg.mozilla.org/users/pmoore_mozilla.com/b2g-manifest-test/',
-    'gecko_push_url': 'ssh://hg.mozilla.org/users/pmoore_mozilla.com/b2g-manifest-test/',
     'manifests_repo': 'B2G_MANIFEST_GIT_URL',
     'manifests_revision': 'B2G_MANIFEST_COMMIT',
     'skip_gaia_json': True,
