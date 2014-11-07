cb1f04cbdca541a71c3f275c06cea8bcce75d686
Pete Moore <pmoore@mozilla.com>
2014-11-07 09:44:02 +0100

    stuff

--- validate.sh
+++ validate.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -euxv
 
-/usr/local/bin/hg --version
-
-echo "Installing mozharness..."
 B2GBUMPER_DIR="$(pwd)"
 cd ..
+echo "Installing mozharness..."
 echo "Current directory: '$(pwd)'"
 git clone -b bug1091084 https://github.com/petemoore/build-mozharness mozharness
 echo "Installing tools..."
@@ -23,8 +21,9 @@ function replace {
     rm "${file}.orig"
 }
 
-# update references to hgtool.py and gittool.py to correct locations...
+echo "Replacing references to hgtool.py and gittool.py to versions checked out in tools repo..."
 replace "${B2GBUMPER_DIR}/travis-mozharness-config.py" HGTOOL "${HGTOOL_PATH}"
 replace "${B2GBUMPER_DIR}/travis-mozharness-config.py" GITTOOL "${GITTOOL_PATH}"
 
+echo "Running b2g bumper..."
 mozharness/scripts/b2g_bumper.py -c "${B2GBUMPER_DIR}/travis-mozharness-config.py" --no-check-treestatus --no-commit-manifests
